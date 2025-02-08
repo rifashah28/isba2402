80cecce72791a59ef804d6b00e42d00dc8f67f90
Rifa Safeer Shah <rifa.safeershah@gmail.com>
2025-02-08 10:21:14 -0800

    Add files via upload

---
+++ assignments/assignment 4/Assignment4_questions (1).sql
@@ -0,0 +1,28 @@
+/*Q1.Compute new cases for each day. */
+
+
+
+
+/*Q2.To account for "administrative weekends" with fewer reports or missing data, 
+compute the smoothed rolling average between two preceding days and two following days. */
+
+
+
+
+/*Q3. Fetch latest available per state data from statistics. Note that states may have different latest submission dates. (hint: ROW_NUMBER())*/
+
+
+
+
+/*Q4.Use the "latest data" derived from the above query and demographic information to compute the mortality per 100,000 population.*/
+
+
+
+
+
+/*Q5.Find the biggest spike in new deaths per state. Sort them by the most recent date, then by the count of new deaths. (hint: RANK())*/
+
+
+
+
+

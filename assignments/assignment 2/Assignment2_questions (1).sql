1f2ec649ca02b2a39c79de8ea2241bcf2506c1c5
Rifa Safeer Shah <rifa.safeershah@gmail.com>
2025-02-08 10:16:19 -0800

    Add files via upload

---
+++ assignments/assignment 2/Assignment2_questions (1).sql
@@ -0,0 +1,43 @@
+-- Q1. Find the titles of all movies directed by Steven Spielberg.
+
+
+
+-- Q2. Find the movies names that contain the word "THE"
+
+
+
+
+-- Q3. Find those rating records higher than 3 stars before 2011/1/15 or after 2011/1/20 
+
+
+    
+/* Q4. Some reviewers did rating on the same movie more than once. 
+How many rating records are there with different movie and different reviewer's rating? */
+
+
+-- Q5. Which are the top 3 records with the highest ratings?
+
+
+-- Q6. Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order.
+
+
+-- Q7. Find the titles of all movies that have no ratings.
+
+/* Q8. Some reviewers didn't provide a date with their rating. 
+ Find the names of all reviewers who have ratings with a NULL value for the date. */
+
+
+ 
+/* Q9. Write a query to return the ratings data in a more readable format in only one field(column): 
+"reviewer name, movie title, stars, ratingDate". 
+Assign a new name to the new column as "Review_details"
+Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
+Hint: join three tables, using join twice. 
+Hint: use CONCAT_WS(separator, string1, string2) instead of CONCAT() for creating new column because of NULL values */
+
+
+
+
+ 
+
+ 

301b36ab8aa01279f6de1cd896d3d84fbd909adf
Rifa Safeer Shah <rifa.safeershah@gmail.com>
2025-02-08 10:19:25 -0800

    Add files via upload

---
+++ assignments/assignment 3/Assignment3_questions (1).sql
@@ -0,0 +1,34 @@
+/*1.Find the names of aircraft such that all pilots certified to operate them have salaries more than $80,000. */
+
+
+
+
+/*2.Find the names of employees whose salary is less than the price of the cheapest route from Bangalore to Frankfurt. */
+
+
+
+
+/*3.For all aircraft with cruising range over 1,000 miles,
+find the name of the aircraft and the average salary of all pilots certified for this aircraft.*/
+
+
+
+
+/*4.Identify the routes that can be piloted by every pilot who makes more than $70,000.
+(In other words, find the routes with distance less than the least cruising range of aircrafts driven by pilots who make more than $70,000) */
+
+
+
+
+/*5. Print the names of pilots who can operate planes with cruising range greater than 3,000 miles but are not certified on any Boeing aircraft. */
+
+
+
+
+/*6. Compute the difference between the average salary of a pilot and the average salary of all employees (including pilots).*/
+
+
+
+
+
+/*7. Print the name and salary of every non-pilot whose salary is more than the average salary for pilots.*/
